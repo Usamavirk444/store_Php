9b920b45e9fb21b255499f8f4bb5d5443ca71765
Usama <93817702+Usamavirk444@users.noreply.github.com>
2022-01-08 15:35:39 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,93 @@
+<?php 
+include('./includes/conn.php');
+if ($login_fetch['role']=='admin') {
+  $_SESSION['email']=$email;
+  header("Location:./dash.php");
+}
+
+if (isset($_POST['submit'])) {
+  $email=mysqli_real_escape_string($conn,$_POST['email']);
+  $password=mysqli_real_escape_string($conn,$_POST['password']);
+
+  $login_sql="SELECT * FROM `login` WHERE `login_email`='$email' and `login_pass`='$password'";
+  $login_run=mysqli_query($conn,$login_sql);
+  $login_fetch=mysqli_fetch_array($login_run);
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+
+<!-- auth-login.html  21 Nov 2019 03:49:32 GMT -->
+<?php include("./includes/head.php");?>
+
+
+<body>
+    <div class="loader"></div>
+    <div id="app">
+        <section class="section">
+            <div class="container mt-5">
+                <div class="row">
+                    <div
+                        class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+                        <div class="card card-primary">
+                            <div class="card-header">
+                                <h4>Login</h4>
+                            </div>
+                            <div class="card-body">
+                                <form method="POST" action="#" class="needs-validation" novalidate="">
+                                    <div class="form-group">
+                                        <label for="email">Email</label>
+                                        <input id="email" type="email" class="form-control" name="email" tabindex="1"
+                                            required autofocus>
+                                        <div class="invalid-feedback">
+                                            Please fill in your email
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="d-block">
+                                            <label for="password" class="control-label">Password</label>
+                                           
+                                        </div>
+                                        <input id="password" type="password" class="form-control" name="password"
+                                            tabindex="2" required>
+                                        <div class="invalid-feedback">
+                                            please fill in your password
+                                        </div>
+                                    </div>
+                                   
+                                    <div class="form-group">
+                                        <button type="submit" name="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
+                                            Login
+                                        </button>
+                                    </div>
+                                </form>
+
+
+                            </div>
+                        </div>
+                        <div class="mt-5 text-muted text-center">
+                            Don't have an account? <a href="auth-register.html">Create One</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+    <!-- General JS Scripts -->
+    <script src="assets/js/app.min.js"></script>
+    <!-- JS Libraies -->
+    <!-- Page Specific JS File -->
+    <!-- Template JS File -->
+    <script src="assets/js/scripts.js"></script>
+    <!-- Custom JS File -->
+    <script src="assets/js/custom.js"></script>
+</body>
+
+
+<!-- auth-login.html  21 Nov 2019 03:49:32 GMT -->
+
+</html>
